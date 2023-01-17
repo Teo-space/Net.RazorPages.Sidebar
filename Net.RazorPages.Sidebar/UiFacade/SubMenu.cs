f18b1cf72d6a731a24febd82ac3955ab8b196bb7
iTeo <barmin.andrey.nnov@gmail.com>
2023-01-17 21:20:42 +0300

    Добавьте файлы проекта.

---
+++ Net.RazorPages.Sidebar/UiFacade/SubMenu.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Concurrent;
+using System.Security.Policy;
+
+
+namespace Net.RazorPages.Sidebar.UiFacade;
+
+
+public class SideBar
+{
+
+    public static ConcurrentDictionary<string, SubMenu> SubMenus { get; private set; } = new();
+
+    public static SubMenu AddSubmenu(string subMenuName)
+    {
+
+        var subMenu = new SubMenu(subMenuName);
+
+        SubMenus.TryAdd(subMenuName, subMenu);
+
+        return subMenu;
+
+    }
+
+}
+
+
+public class SubMenu
+{
+
+    public string Name { get; private set; } = "SubMenu";
+
+
+
+    public SubMenu(string name)
+
+    {
+
+        Name = name;
+
+    }
+
+
+
+    public ConcurrentDictionary<string, string> links { get; } = new();
+
+    public SubMenu Addlink(string Name, string url)
+
+    {
+
+        links[Name] = url;
+
+        return this;
+
+    }
+
+
+
+    //public IEnumerable<(string Name, string Url)> GetLinks() => links.Select(x => (Name: x.Key, Url: x.Value));
+
+    public record Link(string Name, string Url);
+
+    public IEnumerable<Link> GetLinks() => links.Select(x => new Link(x.Key, x.Value)).OrderBy(x => x.Name);
+
+
+
+
+
+
+
+
+
+
+
+    public bool Show { get; protected set; } = false;
+
+    public SubMenu SetShow()
+
+    {
+
+        this.Show = true;
+
+        return this;
+
+    }
+
+}
